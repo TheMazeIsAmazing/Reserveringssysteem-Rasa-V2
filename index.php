96e6980330e9c1085082e5e705c470599e7482cf
1028473 - Mees Muller <89028475+TheMazeIsAmazing@users.noreply.github.com>
2023-04-17 20:24:15 +0200

    "done" with basic structure - Added Styling via SCSS (fancy) - Added Script to prevent refresh/leave page - Added Basic elements such as a footer - Changes to navbar

--- index.php
+++ index.php
@@ -12,56 +12,52 @@ if (isset($_GET['errors'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reserveren - Rasa Senang</title>
     <link rel="stylesheet" type="text/css" href="./includes/css/style.css"/>
-<!--    <script type="text/javascript" src="js/main.js" defer></script>-->
-
+    <script type="text/javascript" src="./includes/js/navBar.js" defer></script>
+    <script type="text/javascript" src="./includes/js/preventUnload.js" async></script>
 </head>
 <body>
 <header>
-    <div class="wrapper">
+    <nav class="wrapper">
         <div class="logo">
-            <a href="#"><img src="logo.png" alt="DevBabu.com"></a>
+            <a href="#"><img src="includes/logo's/logo.jpg" alt="DevBabu.com"></a>
         </div>
         <div class="navbar">
-            <div class="close-nav"><button>×</button></div>
+            <div class="close-nav">
+                <button>×</button>
+            </div>
             <nav>
                 <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Blog</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Contact</a></li>
+                    <li><a href="http://www.rasasenang.com">Hoofdsite</a></li>
+                    <li><a class="current" href="#">Reserveren</a></li>
+                    <li><a href="#">Reservering Wijzigen</a></li>
+                    <li><a href="#">Account</a></li>
                 </ul>
             </nav>
         </div>
         <div class="menu-bar">
             <button><i></i></button>
         </div>
-    </div>
+    </nav>
 </header>
-<div class="container">
-    <!-- Content -->
-</div>
-<script>
-    const theBody = document.querySelector('body');
-    const openNav = document.querySelector('.menu-bar button');
-    const closeNav = document.querySelector('.close-nav button');
-    const Navbar = document.querySelector('.navbar');
-
-    // function bodyScroll(){
-    //     if(Navbar.classList.contains('show')){
-    //         theBody.classList.add('hide-scroll');
-    //     }
-    //     else if(theBody.classList.contains('hide-scroll')){
-    //         theBody.classList.remove('hide-scroll');
-    //     }
-    // }
-
-    function showHide(){
-        Navbar.classList.toggle('show');
-        // bodyScroll();
-    }
-
-    openNav.onclick = showHide;
-    closeNav.onclick = showHide;
-</script>
+<main>
+    <div class="container">
+        <h1>Reserveren</h1>
+        <div>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Eget gravida cum sociis natoque penatibus et magnis. Ipsum a arcu cursus vitae congue
+            mauris. Quis hendrerit dolor magna eget est lorem. Adipiscing diam donec adipiscing tristique risus nec
+            feugiat in. Suspendisse potenti nullam ac tortor vitae. Et molestie ac feugiat sed lectus vestibulum mattis.
+            Ut tellus elementum sagittis vitae. Magna sit amet purus gravida quis blandit. Nibh venenatis cras sed felis
+            eget velit. Sit amet nisl suscipit adipiscing. Id ornare arcu odio ut sem nulla. Etiam sit amet nisl purus.
+            Neque viverra justo nec ultrices.
+        </div>
+    </div>
+    <div class="footer container">
+        <section>
+            <div>© <?= date("Y") ?> - Rasa Senang Dordrecht.</div>
+            <div><a href="./privacystatement"> Privacystatement </a></div>
+        </section>
+    </div>
+</main>
 </body>
 </html>
